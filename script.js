aa327db13edf25b65e336df92bac9ab49035e001
Shivam Soni <78346328+shivam-soni156@users.noreply.github.com>
2022-06-28 11:10:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+var clicks = 0;
+var lastClick = [0, 0];
+
+document.getElementById('canvas').addEventListener('click', drawLine, false);
+
+function getCursorPosition(e) {
+    var x;
+    var y;
+
+    if (e.pageX != undefined && e.pageY != undefined) {
+        x = e.pageX;
+        y = e.pageY;
+    } else {
+        x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+        y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+
+    return [x, y];
+}
+
+function drawLine(e) {
+    context = this.getContext('2d');
+
+    x = getCursorPosition(e)[0] - this.offsetLeft;
+    y = getCursorPosition(e)[1] - this.offsetTop;
+
+    if (clicks != 1) {
+        clicks++;
+    } else {
+        context.beginPath();
+        context.moveTo(lastClick[0], lastClick[1]);
+        context.lineTo(x, y, 6);
+
+        context.strokeStyle = CanvasGradient;
+        context.stroke();
+
+        clicks = 0;
+    }
+
+    lastClick = [x, y];
+};
